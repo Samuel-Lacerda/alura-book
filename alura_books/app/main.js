e7b5e5455d351e1d08fa63cc7ea25a960c4d1b99
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-07-27 11:42:08 -0300

    update

--- alura_books/app/main.js
+++ alura_books/app/main.js
@@ -1,28 +1,12 @@
 let livros = []
 const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaApi()
-const elementoParaInserirLivros = document.querySelector("#livros")
 
 
 async function getBuscarLivrosDaApi() {
     const res = await fetch(endpointDaApi)
     livros = await res.json()
-    console.table(livros)
-    exibirLivrosNaTela(livros)
+    let livroComDesconto = aplicarDesconto(livros)
+    exibirLivrosNaTela(livroComDesconto)
 }
 
-function exibirLivrosNaTela(ListaDeLivros){
-    ListaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `<div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-      <h2 class="livro__titulo">
-        ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco}</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>`
-    })
-}
